db6aa1ba80304e9f65cbf118b2058bbfd90a881c
Carlos <carlosh.serrado@gmail.com>
2024-07-25 10:20:07 -0300

    Protótipo telescópio movendo

---
+++ scripts/background.gd
@@ -0,0 +1,22 @@
+extends TextureRect
+
+@export var speed : float = 10.0
+@export var radius : float = 192.0
+
+func _ready():
+	print(size)
+
+func _process(delta):
+	move()
+
+func move():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var new_pos : Vector2 = position - input_direction.normalized() * speed
+	position = clamp_pos(new_pos)
+	print(position)
+
+func clamp_pos(pos):
+	var new_pos : Vector2
+	new_pos.x = clamp(pos.x, -size.x + get_viewport_rect().size.x, 0)
+	new_pos.y = clamp(pos.y, -size.y + get_viewport_rect().size.y, 0)
+	return new_pos
