15e74bc2ce29da0a8d614d1bb4a94de9bd9b4b98
Carlos <carlosh.serrado@gmail.com>
2024-07-28 16:59:05 -0300

    Força primeiro puzzle

--- scripts/notebook.gd
+++ scripts/notebook.gd
@@ -4,14 +4,18 @@ extends Control
 var puzzles : Array[Node]
 
 func _ready():
+	hide()
 	puzzles = $Notebook/Puzzles.get_children()
 	Progress.checkpoint_reached.connect(_on_checkpoint_reached)
+	GameStatus.enable_notebook.connect(show)
 
 func _on_notebook_button_pressed():
 	if GameStatus.current_state == GameStatus.GAME_STATE.EXPLORATION:
 		animation_player.play("open_notebook_menu")
 		GameStatus.change_state(GameStatus.GAME_STATE.NOTEBOOK)
 
+func _on_first_puzzle_completed():
+	show()
 
 func _on_close_button_pressed():
 	animation_player.play("close_notebook_menu")
